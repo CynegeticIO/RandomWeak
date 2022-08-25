19dd520f3b2758b182981de9fdefb0c88d3727ba
CY0xZ <103884082+CY0xZ@users.noreply.github.com>
2022-08-25 16:09:11 +0200

    Update ETHWalletGeneratorFast.py

--- Module 1 Decentralization/ETHWalletGeneratorFast.py
+++ Module 1 Decentralization/ETHWalletGeneratorFast.py
@@ -28,3 +28,15 @@ for i in range(0,1000000):
   
 adr_df = pd.DataFrame(adr,columns=["Adr"])
 pk_df = pd.DataFrame(pk,columns=["Pk"])
+
+
+
+import sha3
+from ecdsa import SigningKey, SECP256k1
+
+keccak = sha3.keccak_256()
+private = SigningKey.generate(curve=SECP256k1)
+public = private.get_verifying_key().to_string()
+keccak.update(public)
+address = "0x{}".format(keccak.hexdigest()[24:])
+print(address)
